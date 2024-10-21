18ae6a33acc5b5e82f5bfcecdb64aaff573b2a46
RenPen3 <wrenwiththepen@gmail.com>
2024-10-21 00:16:07 -0700

    Update on Framework

--- addImage/addImage.js
+++ addImage/addImage.js
@@ -2,14 +2,23 @@ const addBtn1 = document.querySelector('.btn1');
 const addBtn2 = document.querySelector('.btn2');
 // const addBtn3 = document.querySelector('.btn3');
 const allBtns = document.querySelectorAll('.btn');
-const imageOvrLay = document.getElementsByTagName('img');
 const clrAll = document.querySelector('.clear-all');
 const urlInput = document.getElementById('url');
+const imageURL = "https://i.ibb.co/qDxS6W6/test-img.jpg";
+const image = new Image();
+image.src = imageURL;
+const imageOvrLay = document.getElementsByTagName('.image-overlay');
+imageOvrLay.appendChild(image);
+
+
+
+
 
 
 addBtn1.addEventListener('click', () => {
   console.log('add button 1')
 })
+
 addBtn2.addEventListener('click', () => {
   console.log('add button 2')
 })
